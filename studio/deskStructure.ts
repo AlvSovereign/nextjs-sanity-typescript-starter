6c1fc5a638f4236971ea2e28d7ddb40bd416eaaa
AlvSovereign <web@alvinsovereign.com>
2021-02-20 20:49:04 +0000

    added site data in sanity

---
+++ studio/deskStructure.ts
@@ -0,0 +1,17 @@
+import S from '@sanity/desk-tool/structure-builder';
+
+export default () =>
+  S.list()
+    .title('Base')
+    .items([
+      S.listItem()
+        .title('Settings')
+        .child(
+          S.document().schemaType('siteSettings').documentId('siteSettings')
+        ),
+      S.divider(),
+      // removes siteSettings from studio view
+      ...S.documentTypeListItems().filter(
+        (item) => !['siteSettings'].includes(item.getId())
+      ),
+    ]);
---
+++ studio/schemas/siteSettings.ts
@@ -0,0 +1,18 @@
+export default {
+  name: 'siteSettings',
+  title: 'Site Settings',
+  type: 'document',
+  __experimental_actions: ['update', 'publish'],
+  fields: [
+    {
+      name: 'title',
+      title: 'Site title',
+      type: 'string',
+    },
+    {
+      name: 'description',
+      title: 'Meta description',
+      type: 'text',
+    },
+  ],
+};
